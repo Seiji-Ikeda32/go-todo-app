d13c1a3cf487e49ae6d72879aa0e8da8fa8f3f5b
池田聖示 <ikeda-seiji@ikechan.local>
2022-02-07 23:10:35 +0900

    todo fileの追加とmodelの構造体定義

---
+++ backend/models/todos.go
@@ -0,0 +1,13 @@
+package models
+
+import "time"
+
+type Todo struct {
+	Id          int       `json:"id"`
+	Title       string    `json:"title"`
+	Discription string    `json:"discription"`
+	IsCompleted bool      `json:"is_completed"`
+	DueTime     time.Time `json:"due_time"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
